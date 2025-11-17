14702c43240b86daf93af5803d02de98ea0d70ec
арбузру <adadabaev784@gmail.com>
2025-11-17 21:32:15 +0500

    first commit

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,16 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ServiceController;
+use App\Http\Controllers\Api\OrderController;
+use App\Http\Controllers\Api\ReviewController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('services', ServiceController::class);
+Route::apiResource('orders', OrderController::class);
+Route::apiResource('reviews', ReviewController::class);
